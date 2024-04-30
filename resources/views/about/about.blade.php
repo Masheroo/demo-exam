6f5e36c9f78e15cf4996102f4124bf6644aa2c10
mashero <dima.vuynov@yandex.ru>
2024-04-30 21:27:09 +0300

    30.04.2024

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,8 @@
+@extends('base', ['page_title' => 'О нас'])
+
+@section('content')
+
+<div class="slider">
+    <img src="/images/logo.jpg" alt="">
+</div>
+@stop
---
+++ resources/views/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="logo-container">
+        <a class="logo" href="/"><h1>KuruKuru</h1></a>
+        <h2 class="slogan">Кто носит фирму адидас - тот круче Путина в 100 раз</h2>
+    </div>
+    <div class="links">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+        <a href="">Link 4</a>
+        <a href="">Link 4</a>
+        <a href="">Link 4</a>
+    </div>
+</header>
